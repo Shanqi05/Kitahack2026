b407884af99ec5a402682fd80098592fe0dd93a9
Choo Xuan <52617167+adrianchxuan@users.noreply.github.com>
2026-02-26 03:05:24 +0800

    git ignore fix

--- sleepnotfound404/lib/firebase_options.dart
+++ sleepnotfound404/lib/firebase_options.dart
@@ -28,10 +28,13 @@ class DefaultFirebaseOptions {
   }
 
   static const FirebaseOptions web = FirebaseOptions(
-    apiKey: 'AIzaSyD_uZ5GiImGwscC0Ow7PD7HqWTfpwn4-ks',
+    apiKey: 'AIzaSyA7NGRKA-3v6YYfCVcao2KcSnnZqC9YhoI',
     appId: '1:1067526580765:web:a3b61a654dfdf285eef4f4',
     messagingSenderId: '1067526580765',
     projectId: 'sleepnotfound404-46ce5',
+    authDomain: 'sleepnotfound404-46ce5.firebaseapp.com',
+    storageBucket: 'sleepnotfound404-46ce5.firebasestorage.app',
+    measurementId: 'G-903E0ZYTDD',
   );
 
   static const FirebaseOptions android = FirebaseOptions(
@@ -43,26 +46,33 @@ class DefaultFirebaseOptions {
   );
 
   static const FirebaseOptions ios = FirebaseOptions(
-    apiKey: 'AIzaSyC3rvG5F3R5m3R5m3R5m3R5m3R5m3R5m3',
+    apiKey: 'AIzaSyBM8n5PJ_BuFLbSwSO2tHu7DB8t2YBSLb0',
     appId: '1:1067526580765:ios:08cccfbd81f5b5a0eef4f4',
     messagingSenderId: '1067526580765',
     projectId: 'sleepnotfound404-46ce5',
     storageBucket: 'sleepnotfound404-46ce5.firebasestorage.app',
+    iosClientId: '1067526580765-180lcqi47nferv1jg2uvlk989r7hsev8.apps.googleusercontent.com',
+    iosBundleId: 'com.example.sleepnotfound404',
   );
 
   static const FirebaseOptions macos = FirebaseOptions(
-    apiKey: 'AIzaSyC3rvG5F3R5m3R5m3R5m3R5m3R5m3R5m3',
+    apiKey: 'AIzaSyBM8n5PJ_BuFLbSwSO2tHu7DB8t2YBSLb0',
     appId: '1:1067526580765:ios:08cccfbd81f5b5a0eef4f4',
     messagingSenderId: '1067526580765',
     projectId: 'sleepnotfound404-46ce5',
     storageBucket: 'sleepnotfound404-46ce5.firebasestorage.app',
+    iosClientId: '1067526580765-180lcqi47nferv1jg2uvlk989r7hsev8.apps.googleusercontent.com',
+    iosBundleId: 'com.example.sleepnotfound404',
   );
 
   static const FirebaseOptions windows = FirebaseOptions(
-    apiKey: 'AIzaSyD_uZ5GiImGwscC0Ow7PD7HqWTfpwn4-ks',
+    apiKey: 'AIzaSyA7NGRKA-3v6YYfCVcao2KcSnnZqC9YhoI',
     appId: '1:1067526580765:web:1e84cd04eb5b4f95eef4f4',
     messagingSenderId: '1067526580765',
     projectId: 'sleepnotfound404-46ce5',
+    authDomain: 'sleepnotfound404-46ce5.firebaseapp.com',
+    storageBucket: 'sleepnotfound404-46ce5.firebasestorage.app',
+    measurementId: 'G-1RZT1ECF7C',
   );
 
   static const FirebaseOptions linux = FirebaseOptions(
